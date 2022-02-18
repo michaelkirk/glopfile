8db9414fb21dd587e12e4f0bce5a8fb2313252f0
Michael Kirk <michael.code@endoftheworl.de>
2022-02-17 20:16:08 -0800

    point to published server

--- www-client/sendfile-ui/src/App.tsx
+++ www-client/sendfile-ui/src/App.tsx
@@ -5,7 +5,7 @@ import Downloader from "./Downloader";
 import Uploader from "./Uploader";
 
 class AppState {
-  apiEndpoint = new URL("http://localhost:8080");
+  apiEndpoint = new URL("https://sendfile.jessa0.com")
 }
 
 class App extends React.Component<{}, AppState> {
