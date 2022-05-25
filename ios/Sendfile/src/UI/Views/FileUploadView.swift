b5344cb1efef72d3d7c16824da4c242ded83f0bd
Michael Kirk <michael.code@endoftheworl.de>
2022-05-25 16:16:54 -0700

    Display QR code with download link

--- ios/Sendfile/src/UI/Views/FileUploadView.swift
+++ ios/Sendfile/src/UI/Views/FileUploadView.swift
@@ -7,17 +7,13 @@ struct FileUploadView: View {
     @StateObject var fileUpload: FileUploadModel = FileUploadModel()
 
     var body: some View {
-        ZStack(alignment: .center) {
-            VStack {
-                if case .error(_) = fileUpload.state {
-                    retryButton
-                }
-                uploadStatus
+        VStack(alignment: .center, spacing: 16) {
+            uploadStatus
+            if case .error(_) = fileUpload.state {
+                retryButton
             }
-            VStack {
-                Spacer()
-                provisionedUrlButton
-            }
+            shareView
+            Spacer()
         }
         .onAppear {
             if let fileUrl = fileUrl, case .idle = fileUpload.state {
@@ -26,19 +22,26 @@ struct FileUploadView: View {
         }
     }
 
-    var provisionedUrlButton: some View {
+    var shareView: some View {
         VStack {
             if case .success = fileUpload.state {
-            } else if let provisionedUrl = fileUpload.pending?.provisionedFile?.formattedDownloadUrlAndKey() {
-                Text("Tap to copy to clipboard:")
+            } else if let downloadUrl = fileUpload.pending?.provisionedFile?.formattedDownloadUrlAndKey() {
+                Text("Send this download link to the recipient to continue uploading:")
                 Button(action: {
-                    UIPasteboard.general.url = URL(string: provisionedUrl)!
+                    UIPasteboard.general.url = URL(string: downloadUrl)!
                 }) {
-                    Text("\(provisionedUrl)")
+                    Text("\(downloadUrl)")
+                }.padding(.bottom, 30)
+                
+                if let image = buildQRImage(urlString: downloadUrl) {
+                    Text("Or have them scan this code:")
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(minWidth: nil, idealWidth: nil, maxWidth: 200, minHeight: nil, idealHeight: nil, maxHeight: 200, alignment: .center)
                 }
             }
         }
-        .padding()
     }
 
     var retryButton: some View {
@@ -54,21 +57,48 @@ struct FileUploadView: View {
     }
 
     var uploadStatus: some View {
-        VStack {
+        func Header(_ text: String) -> some View {
+            return Text(text).font(.headline)
+        }
+        return VStack {
             switch fileUpload.state {
             case .idle:
                 EmptyView()
             case .started:
                 if fileUpload.pending == nil {
-                    Text("Provisioning...")
+                    Header("Provisioning...")
                 } else {
-                    Text("Uploading...")
+                    Header("Uploading...")
                 }
             case .success:
-                Text("File sent!")
+                Header("File sent!")
             case .error(let lastError):
-                Text("Error: \(lastError.localizedDescription)")
+                Header("Error: \(lastError.localizedDescription)")
             }
         }
     }
+}
+
+func buildQRImage(urlString: String) -> UIImage? {
+    guard let filter = CIFilter(name: "CIQRCodeGenerator") else {
+        assertionFailure("failed to build QR Code generator")
+        return nil
+    }
+    
+    let data = urlString.data(using: .utf8)
+    filter.setValue(data, forKey: "inputMessage")
+    
+    let scaleUp = CGAffineTransform(scaleX: 4, y: 4)
+    guard let ciImage = filter.outputImage?.transformed(by: scaleUp) else {
+        assertionFailure("failed to generate QR Code image")
+        return nil
+    }
+    
+    guard let renderedImage = CIContext().createCGImage(ciImage, from: ciImage.extent) else {
+        assertionFailure("failed to render QR Code image")
+        return nil
+    }
+    
+    let image = UIImage(cgImage: renderedImage)
+    return image
 }
